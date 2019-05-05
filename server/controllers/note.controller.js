90583eed51af596e724982fb6258da1498a60a62
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-05-06 00:19:18 +0200

    Add - new note and hook on the method find

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -5,3 +5,30 @@ export function getSomething(req, res) {
   return res.status(200).end();
 }
 
+export function addNote(req, res) {
+  const { note, laneId } = req.body;
+
+  if (!note || !note.task || !laneId) {
+    res.status(400).end();
+  }
+
+  const newNote = new Note({
+    task: note.task,
+  });
+
+  newNote.id = uuid();
+  newNote.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    Lane.findOne({ id: laneId })
+      .then(lane => {
+        lane.notes.push(saved);
+        return lane.save();
+      })
+      .then(() => {
+        res.json(saved);
+      });
+  });
+}
+
